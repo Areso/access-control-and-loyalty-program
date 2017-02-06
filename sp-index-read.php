d5bd4476deb64d23a0bd2de8ed7e46eaec2f5480
Anton Gladyshev <returnstrike@yandex.ru>
2017-02-06 16:55:58 +0600

    minor

--- sp-index-read.php
+++ sp-index-read.php
@@ -4,21 +4,24 @@
 </head>
 <body>
 <?php
-echo $_POST["Fingerprint"];
-$fingerpint = $_POST["fingerprint"];
+//echo $_POST["Fingerprint"];
+//$fingerpint = $_POST["fingerprint"];
 $fingerpint = "08-ED-B9-49-B2-E5-1024";
 include("connection.php");
 try {
     $conn = new PDO("mysql: host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT * from main WHERE mac='".$fingerpint."'";
-    //echo $sql;
-    $conn->exec($sql);
+    echo $sql;
+    //$conn->exec($sql);
+	$result  =  $conn->query($sql);
+	$max_row =  $result->fetch(PDO::FETCH_ASSOC);
+	echo $max_row;
     //echo $conn;
     unset($username);
     unset($password);
     
-    echo "result is ";
+    //echo "result is ";
 }
 catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
